0a0a0365d73d08369705e24110813ffff1b8c405
LukeSchaffel <lukeschaffel@gmail.com>
2022-02-27 12:55:09 -0500

    broke db

--- controllers/flights.js
+++ controllers/flights.js
@@ -14,9 +14,24 @@ function newMovie(req, res) {
   res.render('flights/new')
 }
 
+function create(req, res) {
+  const flight = new Flight(req.body)
+  console.log(flight);
+  flight.save(function(err){
+    if (err) return res.redirect('/flights/new')
+    res.redirect('/flights')
+  })
+ 
+
+  }
+  
+
+
+
 
 export {
   index,
-  newMovie as new
+  newMovie as new,
+  create
 
 }
